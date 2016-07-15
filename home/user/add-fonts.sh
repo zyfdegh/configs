1e79b85310bbeda7238bfa6f8a548f0712a20e8c
zyfdegh <zyfdegg@gmail.com>
2016-07-15 20:09:41 +0800

    add fonts

---
+++ home/user/add-fonts.sh
@@ -0,0 +1,14 @@
+# download
+# unzip
+
+# mkdir
+mkdir /usr/share/fonts/consolas
+
+# cp
+cp consolas*.ttf /usr/share/fonts/consolas
+
+# enable
+cd /usr/share/fonts/consolas
+mkfontscale
+mkfontdir
+fc-cache
